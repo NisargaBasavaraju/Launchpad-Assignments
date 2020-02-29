54a5b9df76b7b2478a51310a57ea353f67fda042
NisargaBasavaraju <60998577+NisargaBasavaraju@users.noreply.github.com>
2020-02-29 14:17:36 +0530

    Add files via upload

---
+++ problem6.py
@@ -0,0 +1,14 @@
+addName =input("Enter Name:")
+addid = input("enter usn:")
+dictionary = {}
+dictionary[addid] = addName
+addName1 =input("enter name:")
+addid1 =input("enter usn")
+dictionary[addid1] = addName1
+addName2 =input("enter name:")
+addid2 =input("enter usn")
+dictionary[addid2] = addName2
+print(dictionary)
+
+
+   
